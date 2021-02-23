d52eefcd55ee47551772fc09ab0b222c6203576a
kamada <kamada.math@gmail.com>
2021-02-23 22:41:44 +0900

    first commit

---
+++ testocr/RemainingDays/RemainingDays/RemainingDays/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  RemainingDays
+//
+//  Created by 釜田康平 on 2021/02/23.
+//
+
+import SwiftUI
+    
+
+
+struct ContentView: View {
+    @State var name: String  = ""
+    
+    var body: some View {
+        VStack{
+            Text("受験日は？")
+            TextField("○月○日", text: $name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 250)
+            
+            if (!name.isEmpty) {
+                Text("\(name)さん、こんにちは！")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testocr/RemainingDays/RemainingDays/RemainingDays/RemainingDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemainingDaysApp.swift
+//  RemainingDays
+//
+//  Created by 釜田康平 on 2021/02/23.
+//
+
+import SwiftUI
+
+@main
+struct RemainingDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
